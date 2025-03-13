445bba089dd59fd424a13bc7a4563daf35783546
BlauFx <blaufxsup@gmail.com>
2025-03-13 23:52:27 +0100

    Let's focus only on 4 byte integer as byte/long are out of scope currently. (Could return tho)

--- src/search.c
+++ src/search.c
@@ -6,21 +6,9 @@
 #include <fcntl.h>
 #include "helper.h"
 
-int read_byte(int fd, unsigned long address) {
-    int value;
-    pread(fd, &value, sizeof(char), address);
-    return value;
-}
-
 int read_int(int fd, unsigned long address) {
     int value;
     pread(fd, &value, sizeof(int), address);
-    return value;
-}
-
-long read_long(int fd, unsigned long address) {
-    long value;
-    pread(fd, &value, sizeof(long), address);
     return value;
 }
 
@@ -39,37 +27,11 @@ void search(int fd, long int start, long int end, long int value, Node *list)
 
     while (address <= end)
     {
-        // long int data = read_byte(fd, address);
-        // if (data == value) {
-        //     printf("Found byte \taddr: 0x%lx with value: %ld\n", address, data);
-        //
-        //     currentNode->address = address;
-        //     currentNode->value = data;
-        //
-        //     currentNode->type = NODE_TYPE_BYTE;
-        //
-        //     currentNode->next = malloc(sizeof(Node));
-        //     currentNode = currentNode->next;
-        // }
-
         data = read_int(fd, address);
 
         if (data == value) {
             // printf("Found integer \taddr: 0x%lx with value: %ld\n", address, data);
             currentNode->type = NODE_TYPE_INT;
-
-            currentNode->address = address;
-            currentNode->value = data;
-
-            currentNode->next = malloc(sizeof(Node));
-            currentNode = currentNode->next;
-        }
-
-        data = read_long(fd, address);
-
-        if (data == value) {
-            // printf("Found long \taddr: 0x%lx with value: %ld\n", address, data);
-            currentNode->type = NODE_TYPE_LONG;
 
             currentNode->address = address;
             currentNode->value = data;
